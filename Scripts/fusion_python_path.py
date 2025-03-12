ec6a30a5228e37a25b696a36acb18085903cb3bf
h1med <mohamedmah53@gmai.com>
2025-03-12 22:04:16 +0100

    richtiges Skrip in Skript

---
+++ Scripts/fusion_python_path.py
@@ -0,0 +1,17 @@
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def run():
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        _PATH = 'HIER PFAD EINGEBEN'
+        import sys
+        app.log(f'sys executable: {sys.executable}')
+        if not _PATH in sys.path:
+            sys.path.append(_PATH)
+            pass
+        app.log(f'sys path: {sys.path[-1]}')
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
